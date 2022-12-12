f958abf7f8ce6f1ae1a3fd162e1854b7c4cf9dbf
张翔 <zhangxiang01@mail.jj.cn>
2022-12-12 18:36:23 +0800

    Merge branch 'main' of github.com:ShawnZhangxxx/go-alg into main
    # Conflicts:
    #	.idea/workspace.xml
    #	liner/slice.go

--- linkList/LinkList.go
+++ linkList/LinkList.go
@@ -1,1 +1,38 @@
 package linkList
+
+import (
+	"unsafe"
+)
+
+type LinkList struct {
+	len   int
+	start *Node
+}
+
+type Node struct {
+	//prev unsafe.Pointer
+	next unsafe.Pointer
+	data int
+}
+
+func (ll *LinkList) Create() {
+	var node = &Node{
+		next: nil,
+		data: nil,
+	}
+	ll.start = node
+}
+func (ll *LinkList) append(data int) {
+	if ll.start == nil {
+		var node = &Node{
+			next: nil,
+			data: data,
+		}
+		ll.start = node
+	} else {
+		for ll.start.next != nil {
+
+		}
+	}
+
+}
